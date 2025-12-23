681a4937a1c0cc91b24e763f1efae3e49ee6b153
LegendsOfXania <legendsofxania@gmail.com>
2025-12-23 10:35:24 +0100

    Update files

--- settings.gradle.kts
+++
@@ -1,9 +0,0 @@
-pluginManagement {
-    repositories {
-        mavenCentral()
-        gradlePluginPortal()
-        maven("https://maven.typewritermc.com/beta")
-    }
-}
-
-rootProject.name = "DungeonExtension"
