e0fe374d26f9a25a2e1bd5170cece690c8aa3552
LegendsOfXania <legendsofxania@gmail.com>
2025-12-28 20:52:15 +0100

    Update interaction

--- extension/src/main/kotlin/fr/legendsofxania/dungeon/interactions/dungeon/DungeonInteraction.kt
+++ extension/src/main/kotlin/fr/legendsofxania/dungeon/interactions/dungeon/DungeonInteraction.kt
@@ -5,11 +5,17 @@ import com.typewritermc.core.interaction.InteractionContext
 import com.typewritermc.core.utils.ok
 import com.typewritermc.engine.paper.entry.entries.EventTrigger
 import com.typewritermc.engine.paper.entry.triggerFor
+import com.typewritermc.engine.paper.utils.toBukkitLocation
 import fr.legendsofxania.dungeon.entries.action.StartDungeonInstanceActionEntry
+import fr.legendsofxania.dungeon.events.AsyncPlayerJoinDungeonInstanceEvent
+import fr.legendsofxania.dungeon.events.AsyncPlayerLeaveDungeonInstanceEvent
 import fr.legendsofxania.dungeon.interactions.dungeon.trigger.DungeonStopTrigger
 import fr.legendsofxania.dungeon.managers.InstanceManager
 import fr.legendsofxania.dungeon.managers.PlayerManager
+import fr.legendsofxania.dungeon.managers.StructureManager
+import fr.legendsofxania.dungeon.managers.WorldManager
 import org.bukkit.entity.Player
+import org.bukkit.event.player.PlayerTeleportEvent
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import java.time.Duration
@@ -23,17 +29,35 @@ class DungeonInteraction(
 ) : Interaction, KoinComponent {
     private val instanceManager: InstanceManager by inject()
 
+    private val dungeonEntry = entry.dungeon
+    private val dungeonLocation = WorldManager.startDungeon()
+    private val dungeonInstance = instanceManager.startDungeonInstance(dungeonEntry, dungeonLocation)
+
     override suspend fun initialize(): Result<Unit> {
+        StructureManager(instanceManager).placeRooms(player, context, dungeonInstance)
+        PlayerManager(instanceManager).setDungeonInstance(player, dungeonInstance)
+
+        val world = dungeonLocation.world
+            ?: error("Dungeon world not found")
+        val coordinate = dungeonEntry.entry?.respawnLocation?.get(player, context)?.toBukkitLocation(world)
+            ?: error("Spawn location not found for DungeonDefinitionEntry: ${dungeonEntry.id}")
+
+        player.teleportAsync(coordinate, PlayerTeleportEvent.TeleportCause.PLUGIN)
+
+        AsyncPlayerJoinDungeonInstanceEvent(player, dungeonEntry).callEvent()
 
         return ok(Unit)
     }
 
     override suspend fun tick(deltaTime: Duration) {
-
          if (PlayerManager(instanceManager).getDungeonInstance(player) == null) DungeonStopTrigger.triggerFor(player, context)
     }
 
     override suspend fun teardown() {
+        StructureManager(instanceManager).deleteRooms(dungeonInstance)
+        WorldManager.stopDungeon(dungeonLocation)
+        instanceManager.stopDungeonInstance(dungeonInstance)
 
+        AsyncPlayerLeaveDungeonInstanceEvent(player, dungeonEntry).callEvent()
     }
 }
