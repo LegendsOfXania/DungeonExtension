f1256dca523928c94805b1a8775388d02f0f3e8c
LegendsOfXania <legendsofxania@gmail.com>
2025-12-29 20:47:16 +0100

    Add InDungeonAudienceEntry & InRoomAudienceEntry

---
+++ extension/src/main/kotlin/fr/legendsofxania/dungeon/entries/manifest/audience/InDungeonAudienceEntry.kt
@@ -0,0 +1,71 @@
+package fr.legendsofxania.dungeon.entries.manifest.audience
+
+import com.typewritermc.core.books.pages.Colors
+import com.typewritermc.core.entries.Ref
+import com.typewritermc.core.entries.emptyRef
+import com.typewritermc.core.entries.ref
+import com.typewritermc.core.extension.annotations.Entry
+import com.typewritermc.engine.paper.entry.entries.AudienceEntry
+import com.typewritermc.engine.paper.entry.entries.AudienceFilter
+import com.typewritermc.engine.paper.entry.entries.AudienceFilterEntry
+import com.typewritermc.engine.paper.entry.entries.ConstVar
+import com.typewritermc.engine.paper.entry.entries.Invertible
+import com.typewritermc.engine.paper.entry.entries.Var
+import com.typewritermc.engine.paper.interaction.interactionContext
+import fr.legendsofxania.dungeon.entries.manifest.definition.DungeonDefinitionEntry
+import fr.legendsofxania.dungeon.events.AsyncPlayerJoinDungeonInstanceEvent
+import fr.legendsofxania.dungeon.events.AsyncPlayerLeaveDungeonInstanceEvent
+import fr.legendsofxania.dungeon.managers.InstanceManager
+import fr.legendsofxania.dungeon.managers.PlayerManager
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+@Entry(
+    "in_room_audience",
+    "Return if a player is in a RoomInstance or not.",
+    Colors.GREEN,
+    "tabler:building"
+)
+/**
+ * The `In Dungeon Audience` entry is used to check if a player is currently in a DungeonInstance.
+ *
+ * ## How could this be used?
+ *
+ * This could be used to apply effects to players who are in a dungeon.
+ */
+class InDungeonAudienceEntry(
+    override val id: String = "",
+    override val name: String = "",
+    override val children: List<Ref<out AudienceEntry>> = emptyList(),
+    val dungeon: Var<Ref<DungeonDefinitionEntry>> = ConstVar(emptyRef()),
+    override val inverted: Boolean = false
+) : AudienceFilterEntry, Invertible {
+    override suspend fun display(): AudienceFilter = InDungeonAudienceFilter(ref(), dungeon)
+}
+
+class InDungeonAudienceFilter(
+    ref: Ref<out AudienceFilterEntry>,
+    val dungeon: Var<Ref<DungeonDefinitionEntry>>
+) : AudienceFilter(ref), KoinComponent {
+    private val instanceManager: InstanceManager by inject()
+
+    override fun filter(player: Player): Boolean {
+        val targetDungeon = dungeon.get(player, player.interactionContext)
+        val dungeonInstance = PlayerManager(instanceManager).getDungeonInstance(player)
+
+        return if (targetDungeon.isSet) dungeonInstance?.definition == targetDungeon
+        else dungeonInstance != null
+    }
+
+    @EventHandler
+    fun onPlayerJoinDungeonInstance(event: AsyncPlayerJoinDungeonInstanceEvent) {
+        event.player.refresh()
+    }
+
+    @EventHandler
+    fun onPlayerLeaveDungeonInstance(event: AsyncPlayerLeaveDungeonInstanceEvent) {
+        event.player.refresh()
+    }
+}
---
+++ extension/src/main/kotlin/fr/legendsofxania/dungeon/entries/manifest/audience/InRoomAudienceEntry.kt
@@ -0,0 +1,71 @@
+package fr.legendsofxania.dungeon.entries.manifest.audience
+
+import com.typewritermc.core.books.pages.Colors
+import com.typewritermc.core.entries.Ref
+import com.typewritermc.core.entries.emptyRef
+import com.typewritermc.core.entries.ref
+import com.typewritermc.core.extension.annotations.Entry
+import com.typewritermc.engine.paper.entry.entries.AudienceEntry
+import com.typewritermc.engine.paper.entry.entries.AudienceFilter
+import com.typewritermc.engine.paper.entry.entries.AudienceFilterEntry
+import com.typewritermc.engine.paper.entry.entries.ConstVar
+import com.typewritermc.engine.paper.entry.entries.Invertible
+import com.typewritermc.engine.paper.entry.entries.Var
+import com.typewritermc.engine.paper.interaction.interactionContext
+import fr.legendsofxania.dungeon.entries.manifest.definition.RoomDefinitionEntry
+import fr.legendsofxania.dungeon.events.AsyncPlayerJoinRoomInstanceEvent
+import fr.legendsofxania.dungeon.events.AsyncPlayerLeaveRoomInstanceEvent
+import fr.legendsofxania.dungeon.managers.InstanceManager
+import fr.legendsofxania.dungeon.managers.PlayerManager
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+@Entry(
+    "in_dungeon_audience",
+    "Return if a player is in a DungeonInstance or not.",
+    Colors.GREEN,
+    "tabler:building-arch"
+)
+/**
+ * The `In Room Audience` entry is used to check if a player is currently in a RoomInstance.
+ *
+ * ## How could this be used?
+ *
+ * This could be used to apply effects to players who are in a dungeon.
+ */
+class InRoomAudienceEntry(
+    override val id: String = "",
+    override val name: String = "",
+    override val children: List<Ref<out AudienceEntry>> = emptyList(),
+    val room: Var<Ref<RoomDefinitionEntry>> = ConstVar(emptyRef()),
+    override val inverted: Boolean = false
+) : AudienceFilterEntry, Invertible {
+    override suspend fun display(): AudienceFilter = InRoomAudienceFilter(ref(), room)
+}
+
+class InRoomAudienceFilter(
+    ref: Ref<out AudienceFilterEntry>,
+    val room: Var<Ref<RoomDefinitionEntry>>
+) : AudienceFilter(ref), KoinComponent {
+    private val instanceManager: InstanceManager by inject()
+
+    override fun filter(player: Player): Boolean {
+        val targetRoom = room.get(player, player.interactionContext)
+        val roomInstance = PlayerManager(instanceManager).getRoomInstance(player)
+
+        return if (targetRoom.isSet) roomInstance?.definition == targetRoom
+        else roomInstance != null
+    }
+
+    @EventHandler
+    fun onPlayerJoinRoomInstance(event: AsyncPlayerJoinRoomInstanceEvent) {
+        event.player.refresh()
+    }
+
+    @EventHandler
+    fun onPlayerLeaveRoomInstance(event: AsyncPlayerLeaveRoomInstanceEvent) {
+        event.player.refresh()
+    }
+}
